8d4c7989b4f1c080bbebdb4b6d4a4c986205e25b
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-08 18:54:16 -0800

    added request-handlers.js

---
+++ request-handlers.js
@@ -0,0 +1,10 @@
+function start() {
+  console.log('request handler "start" was called');
+}
+
+function upload() {
+  console.log('Request handler "upload" was called');
+}
+
+exports.start = start;
+exports.upload = uploat
