42db8e8a63b808cc0ac9977cfd381154f9909614
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-08 21:03:58 -0800

    wrote show handler

--- request-handlers.js
+++ request-handlers.js
@@ -1,5 +1,6 @@
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
 function start(response, postData) {
   console.log('request handler "start" was called');
@@ -10,7 +11,7 @@ function start(response, postData) {
   '</head>' +
   '<body>' +
   '<form action="/upload" enctype="multipart/form-data" method="post">' +
-  '<input type="file" nme="upload" />' +
+  '<input type="file" name="upload" multiple="multiple"/>' +
   '<input type="submit" value="Upload file">'
   '</form>' +
   '</body>' +
@@ -23,10 +24,21 @@ function start(response, postData) {
 
 function upload(response, postData) {
   console.log('Request handler "upload" was called');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You sent: ' + 
-  querystring.parse(postData).text);
-  response.end();
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, (error, fields, files) => {
+    console.log('parsing done');
+    fs.rename(files.upload.path, '/tmp/test.png', error => {
+      if(error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png')
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image: <br/>');
+    response.write('<img src="/show">');
+    response.end();
+  });
 }
 
 function show(response) {
--- server.js
+++ server.js
@@ -3,20 +3,9 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest (request, response) {
-    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + 'received.');
-
-    request.setEncoding('utf-8');
-
-    request.addListener('data', (postDataChunk) => {
-      postData += postDataChunk;
-      console.log('Received POST data chunk "' + postDataChunk + '".');
-    });
-
-    request.addListener('end', () => {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
