60bad0e71be346e7c23693a2f34266486af056f5
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-08 18:31:55 -0800

    added console.logs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
 function onRequest (request, response) {
+  console.log('Request received.');
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+console.log('server has started.');
