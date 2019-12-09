5a8a0b6b4ef8c145eb9870c1241bffed0bfb3e8e
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-08 19:32:20 -0800

    typo

--- request-handlers.js
+++ request-handlers.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 function start(response) {
   console.log('request handler "start" was called');
-  expect('ls -lah', (error, stdout, stderr) => {
+  exec('ls -lah', (error, stdout, stderr) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write(stdout);
     response.end;
