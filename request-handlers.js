89010a32e04c66f53afa903bcdd44ae0a041b40d
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-08 20:35:52 -0800

    added handler for showing an image

--- request-handlers.js
+++ request-handlers.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const fs = require('fs');
 
 function start(response, postData) {
   console.log('request handler "start" was called');
@@ -28,5 +29,12 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response) {
+  console.log('Request handler "show" was called');
+  response.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
